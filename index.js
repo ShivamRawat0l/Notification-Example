84f623a4d429f089890f41efed97eb408285cb2b
Shivam Rawat <shivam.rawat@mutualmobile.com>
2022-04-06 12:29:44 +0530

    Altered code

--- index.js
+++ index.js
@@ -2,8 +2,30 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
+import notifee, {AndroidImportance} from '@notifee/react-native';
+
+const displayNotificationIOS = async () => {
+  await notifee.displayNotification({
+    title: 'Your account requires attention',
+    body: 'You are overdue payment on one or more of your accounts!',
+    ios: {
+      foregroundPresentationOptions: {
+        alert: true,
+        badge: true,
+        sound: true,
+      },
+    },
+  });
+};
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+
+  displayNotificationIOS();
+});
 
 AppRegistry.registerComponent(appName, () => App);
